901fa6be93255a5236e7cb9acb579db00f3f9e5a
Lim Kien Seng <lim2418284@gmail.com>
2017-11-14 04:11:04 +0800

    Initial push

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	
+	//Side menu and top menu load function 
+	$('.sidemenu-section').load('assets/html/menu.html');	
+	$('.topmenu-section').load('assets/html/header.html');	
+	
+});
---
+++ assets/js/page-js/dashboard.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	//Notification content setting 	
+	$.notify({
+		icon: 'pe-7s-gift',
+		message: "Hi <b> Username</b>, <br> You have 3 new assignment."
+
+	},{
+		type: 'info',
+		timer: 4000
+	});
+
+});
